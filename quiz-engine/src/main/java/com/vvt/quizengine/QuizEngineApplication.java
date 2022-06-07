0e9b0ba4732d3816aea0de80295aedb112f0688e
Thanh Vo <thanhvv@gmail.com>
2022-06-07 11:01:48 +0700

    Add the initial frame for Quiz Engine.

---
+++ quiz-engine/src/main/java/com/vvt/quizengine/QuizEngineApplication.java
@@ -0,0 +1,13 @@
+package com.vvt.quizengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizEngineApplication.class, args);
+	}
+
+}
---
+++ quiz-engine/src/test/java/com/vvt/quizengine/QuizEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vvt.quizengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
